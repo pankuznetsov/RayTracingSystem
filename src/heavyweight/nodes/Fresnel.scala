9baffe20d7cea1d4d048c7eae69da0b838224739
akuznetsov700 <akuznetsov700@gmail.com>
2019-02-10 23:08:54 +0300

    Some nodes added.

--- src/heavyweight/nodes/Fresnel.scala
+++ src/heavyweight/nodes/Fresnel.scala
@@ -4,7 +4,7 @@ import lightweight.World
 import lightweight.geometry.{Mesh, Ray, Vector3D}
 import lightweight.nodes.{Container, Node}
 
-class Fresnel(override val inputs: Array[Container], override val outputs: Array[Container]) extends Node(inputs, outputs) {
+class Fresnel(override val inputs: Array[Container], override val outputs: Array[Container], val materialIOR: Double) extends Node(inputs, outputs) {
 
   override def doThings(mesh: Mesh, world: World, triangleIndex: Int, ray: Ray, hitPoint: Vector3D, shadersLeft: Int): Unit = {
 
