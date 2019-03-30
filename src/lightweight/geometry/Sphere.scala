2de591789c6ac09989ff1bd3b163a91549ba1810
akuznetsov700 <akuznetsov700@gmail.com>
2019-03-30 09:09:56 +0300

    created Spheres!

--- src/lightweight/geometry/Sphere.scala
+++ src/lightweight/geometry/Sphere.scala
@@ -3,6 +3,28 @@ package lightweight.geometry
 case class Sphere(center: Vector3D, radius: Float) {
 
   def getSphereAndSphereIntersection(otherSphere: Sphere): Boolean = {
-    false
+    (center - otherSphere.center).magnitude < radius + otherSphere.radius
+  }
+
+  def getRayAndSphereIntersection(ray: Ray): Boolean = {
+    (center - (ray.origin + ray.direction * (ray.origin - center).dotProduct(ray.direction))).magnitude < radius
+  }
+
+  def getTriangleBoundingSphere(triangle: Triangle): Sphere = {
+    val sphereCenter = (triangle.a + triangle.b + triangle.c) / 3
+    val distensyA = (triangle.a - sphereCenter).magnitude
+    val distensyB = (triangle.b - sphereCenter).magnitude
+    val distensyC = (triangle.c - sphereCenter).magnitude
+    val farVertix =
+      if (distensyA > distensyB && distensyA > distensyC) {
+        distensyA
+      } else {
+        if (distensyB > distensyA && distensyB > distensyC) {
+          distensyB
+        } else {
+          distensyC
+        }
+      }
+    Sphere(sphereCenter, farVertix)
   }
 }
