0a62252e61df4cae557f65bcc98cf33b522bfeed
pankuznetsov <pankuznetsov@gmail.com>
2020-05-14 21:33:25 +0300

    Update RayOriginInfo.scala

--- src/lightweight/RayOriginInfo.scala
+++ src/lightweight/RayOriginInfo.scala
@@ -2,5 +2,5 @@ package lightweight
 
 import lightweight.nodes.Node
 
-
+// Tells about the origin of ray and thus, it's importance
 case class RayOriginInfo(generator: Node, isCameraRay: Boolean = true)
