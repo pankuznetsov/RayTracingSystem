50b27ac99a8e68b940f07e70fd444b91851b4087
pankuznetsov <pankuznetsov@gmail.com>
2020-05-14 21:32:01 +0300

    Update World.scala

--- src/lightweight/World.scala
+++ src/lightweight/World.scala
@@ -2,4 +2,7 @@ package lightweight
 
 import lightweight.nodes.{SurfaceOutput, VolumeOutput}
 
+/* The world is much like skybox. It has surface which is distant sky or possibly space,
+volume which can be fog or smoke and global IOR - Index Of Refraction.
+*/
 case class World(skySurface: SurfaceOutput, skyVolume: VolumeOutput, worldIOR: Float)
